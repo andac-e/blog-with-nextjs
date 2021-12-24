ad8feeb71e4c6090dfc456089e3f13bbe3016d20
andac-e <andac.erdogmus@etiya.com>
2021-12-24 13:44:24 +0300

    fix: prod auth0

--- pages/_app.js
+++ pages/_app.js
@@ -5,8 +5,8 @@ import { Auth0Provider } from "@auth0/auth0-react";
 function MyApp({ Component, pageProps }) {
   return (
     <Auth0Provider
-      domain="dev-c-jctz0j.us.auth0.com"
-      clientId="Z4iFyg25aQT1x5DuCUq4pwfklR3VFrSS"
+      domain={process.env.NEXT_PUBLIC_AUTH0_DOMAIN}
+      clientId={process.env.NEXT_PUBLIC_AUTH0_CLIENT_ID}
       redirectUri={process.env.NEXT_PUBLIC_URL}
     >
       <div className="antialiased text-gray-700">
